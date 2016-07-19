89ebd83cda05fb995e10b384b68e4fcdc2aa13d0
Vladimir Anokhin <ano.vladimir@gmail.com>
2016-07-19 03:04:06 +0300

    Initial release

---
+++ gravityforms-toolbar-edit-links.php
@@ -0,0 +1,203 @@
+<?php
+/*
+  Plugin Name: Gravity Forms: Toolbar Edit Links
+  Plugin URI: https://github.com/gndev/gravity-forms-toolbar-edit-links/
+  Version: 1.0.0
+  Author: Vladimir Anokhin
+  Author URI: http://gndev.info/
+  Description: Adds "Edit GForm" link to Admin Bar on pages with Gravity Forms shortcode
+  Text Domain: gravity-forms-toolbar-edit-links
+  Domain Path: /languages
+  License: GPL
+ */
+
+
+if ( !class_exists( 'GF_Toolbar_Edit_Links' ) ) {
+
+	/**
+	 * Plugin Class
+	 */
+	final class GF_Toolbar_Edit_Links {
+
+		/**
+		 * Form IDs
+		 *
+		 * @since  1.0.0
+		 * @var mixed
+		 * @access public
+		 */
+		public static $form_IDs;
+
+
+		/**
+		 * Empty constructor
+		 *
+		 * @since  1.0.0
+		 * @return  void
+		 */
+		public function __construct() {}
+
+
+		/**
+		 * Init plugin
+		 *
+		 * @since  1.0.0
+		 * @return void
+		 */
+		public static function init() {
+
+			add_filter( 'gform_shortcode_form', array( __CLASS__, 'gform_shortcode_form' ), 10, 3  );
+			add_action( 'admin_bar_menu',       array( __CLASS__, 'admin_bar_menu' ),       79.797 );
+			add_action( 'wp_head',              array( __CLASS__, 'print_styles' ),         10     );
+		}
+
+
+		/**
+		 * Display admin notice
+		 *
+		 * @since  1.0.0
+		 * @return admin notice markup
+		 */
+		public static function admin_notice() {
+
+			printf(
+				'<div class="notice error"><h4>%s</h4><p>%s</p></div>',
+				__( 'Gravity Forms: Toolbar Edit Links', 'gravity-forms-toolbar-edit-links' ),
+				sprintf( __( 'Plugin could not be initialized because class with the same name (%s) already exists', 'gravity-forms-toolbar-edit-links' ), __CLASS__ )
+			);
+		}
+
+
+		/**
+		 * Hook to [gravityform] shortcode to collect Form IDs
+		 *
+		 * @since  1.0.0
+		 * @param string  $shortcode_string Shortcode string
+		 * @param mixed   $attributes       Shortcode attributes
+		 * @param string  $content          Shortcode content
+		 * @return string                   Unmodified shortcode string
+		 */
+		public static function gform_shortcode_form( $shortcode_string, $attributes, $content ) {
+
+			// Check capability
+			if ( !GFAPI::current_user_can_any( 'gravityforms_edit_forms' ) ) {
+				return $shortcode_string;
+			}
+
+			// Check that Form ID is set properly
+			if ( !isset( $attributes['id'] ) || !is_numeric( $attributes['id'] ) ) {
+				return $shortcode_string;
+			}
+
+			// Add Form ID to global variable
+			self::$form_IDs[] = $attributes['id'];
+
+			// Return unmodified string
+			return $shortcode_string;
+		}
+
+
+		/**
+		 * Add new Toolbar nodes
+		 *
+		 * @since  1.0.0
+		 * @param mixed   $toolbar WP_Admin_bar instance
+		 * @return void
+		 */
+		public static function admin_bar_menu( $toolbar ) {
+
+			// Check there is at least one form on the page
+			if ( empty( self::$form_IDs ) ) {
+
+				return;
+			}
+
+			// Add single node
+			if ( count( self::$form_IDs ) === 1 ) {
+
+				$form_ID = self::$form_IDs[0];
+
+				$args = array(
+					'id'     => 'gravity-forms-toolbar-edit-links-item-' . $form_ID,
+					'href'   => admin_url( 'admin.php?page=gf_edit_forms&id=' . $form_ID ),
+					'title'  => __( 'Edit GForm', 'gravity-forms-toolbar-edit-links' ),
+					'meta'   => array(
+						'class' => 'gravity-forms-toolbar-edit-links-item-top'
+					),
+				);
+
+				// Add Toolbar node
+				$toolbar->add_node( $args );
+			}
+
+			// Add multiple nodes (with parent)
+			elseif ( count( self::$form_IDs ) > 1 ) {
+
+				// Parent node args
+				$args = array(
+					'title' => __( 'Edit GForms', 'gravity-forms-toolbar-edit-links' ),
+					'id'    => 'gravity-forms-toolbar-edit-links-group',
+					'meta'   => array(
+						'class' => 'gravity-forms-toolbar-edit-links-item-top'
+					),
+				);
+
+				// Add parent node
+				$toolbar->add_node( $args );
+
+				// Loop through Form IDs
+				foreach ( self::$form_IDs as $form_ID ) {
+
+					// Get form data
+					$form = GFAPI::get_form( $form_ID );
+
+					// Child node args
+					$args = array(
+						'id'     => 'gravity-forms-toolbar-edit-links-item-' . $form_ID,
+						'parent' => 'gravity-forms-toolbar-edit-links-group',
+						'href'   => admin_url( 'admin.php?page=gf_edit_forms&id=' . $form_ID ),
+						'title'  => $form['title'],
+						'meta'   => array(
+							'class' => 'gravity-forms-toolbar-edit-links-item-sub'
+						),
+					);
+
+					// Add child node
+					$toolbar->add_node( $args );
+				}
+			}
+		}
+
+
+		/**
+		 * Print some CSS to add an icon to the Toolbar node
+		 *
+		 * @since  1.0.0
+		 * @return void
+		 */
+		public static function print_styles() {
+
+			// Check this is front and Toolbar is showing
+			if ( !is_admin() && is_admin_bar_showing() ) {
+				echo "\n<style>.gravity-forms-toolbar-edit-links-item-top>.ab-item:before{content:'\\f464';top:2px}</style>\n";
+			}
+		}
+
+
+	} // END GF_Toolbar_Edit_Links class
+
+	// Init plugin
+	add_action( 'plugins_loaded', array( 'GF_Toolbar_Edit_Links', 'init' ) );
+
+} // END if class_exists check
+
+else {
+
+	add_action( 'admin_notices', array( 'GF_Toolbar_Edit_Links', 'admin_notice' ) );
+} // END class_exists check
+
+
+// Strings for PO Edit
+if ( false ) {
+	__( 'Adds "Edit GForm" link to Admin Bar on pages with Gravity Forms shortcode', 'gravity-forms-toolbar-edit-links' );
+}
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Gravity Forms: Toolbar Edit Links ===
+Contributors: gn_themes
+License: GPL
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=MMA2VA7JDXWDY
+Tags: gravity forms, toolbar, admin bar, edit link, international, multilingual
+Requires at least: 3.5
+Tested up to: 5
+Stable tag: trunk
+
+Adds "Edit GForm" link to Admin Bar on pages with Gravity Forms shortcode
+
+
+== Description ==
+
+This plugin is an add-on for Gravity Froms.
+
+Adds "Edit GForm" link to Admin Bar on pages with Gravity Forms shortcode.
+Multiple forms on same page are also supported.
+
+[Contribute on GitHub](https://github.com/gndev/gravity-forms-toolbar-edit-links/)
+
+
+== Installation ==
+
+Unzip plugin files and upload them under your '/wp-content/plugins/' directory.
+
+Resulted names will be:
+  './wp-content/plugins/gavity-forms-toolbar-edit-links/*'
+
+Activate plugin at "Plugins" administration page.
+
+
+== Upgrade Notice ==
+
+Upgrade normally
+
+
+== Screenshots ==
+
+1. Page with single GForm
+1. Page with multiple GForms
+
+
+== Frequently Asked Questions ==
+
+[Ask author](http://gndev.info/feedback/)
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
